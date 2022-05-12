c5788099170ead8d1694ba24a62ecb3bac2cde51
HrushikC <chiluvurihrushik@gmail.com>
2022-05-12 09:55:34 -0400

    merged sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,67 +1,66 @@
-// alert( 'Hello, world!' );
+// import Tesseract from 'tesseract.js';
 
+
+
+function recognizeText(f) {
+  document.body.append('in progress...\n')
+  document.body.append(document.createElement('br'));
+  Tesseract.recognize(
+  f,// 'https://tesseract.projectnaptha.com/img/eng_bw.png',
+  'eng',
+  { logger: m => console.log(m) }
+  ).then(({ data: { text } }) => {
+    document.body.append(text);
+    console.log(text);
+  })
+}
 
 function dropHandler(ev) {
-  console.log('File(s) dropped');
+console.log('File(s) dropped');
 
-  // Prevent default behavior (Prevent file from being opened)
-  ev.preventDefault();
+// Prevent default behavior (Prevent file from being opened)
+ev.preventDefault();
 
-  if (ev.dataTransfer.items) {
-    // Use DataTransferItemList interface to access the file(s)
-    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-      // If dropped items aren't files, reject them
-      if (ev.dataTransfer.items[i].kind === 'file' && (ev.dataTransfer.items[i].type.match('^image/'))) {
-        var file = ev.dataTransfer.items[i].getAsFile();
-        console.log('... file[' + i + '].name = ' + file.name);
-        // processImg(file)
+if (ev.dataTransfer.items) {
+  // Use DataTransferItemList interface to access the file(s)
+  for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+    // If dropped items aren't files, reject them
+    if (ev.dataTransfer.items[i].kind === 'file') {
+      var file = ev.dataTransfer.items[i].getAsFile();
+      recognizeText(file);
+      console.log('... file[' + i + '].name = ' + file.name);
 
-        const img = document.createElement("img");
-        img.src = URL.createObjectURL(file);
-        img.height = 600, img.width = 600; // Fix image resizing
+      // const img = document.createElement("img");
+      // img.src = URL.createObjectURL(file);
+      // img.height = 600, img.width = 600; // Fix image resizing
 
-        // Placed Canvas under tester div to see how image changed
-        const div = document.getElementById("tester");
+      // // Placed Canvas under tester div to see how image changed
+      // const div = document.getElementById("tester");
 
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext("2d");
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.filter = "contrast(200%) grayscale()";
-        img.onload = function(){
-          ctx.drawImage(img, 0, 0);
-        }
+      // const canvas = document.createElement("canvas");
+      // const ctx = canvas.getContext("2d");
+      // canvas.width = img.width;
+      // canvas.height = img.height;
+      // ctx.filter = "contrast(200%) grayscale()";
+      // img.onload = function(){
+      //   ctx.drawImage(img, 0, 0);
+      // }
 
-        div.appendChild(canvas);
-
-      }
+      // div.appendChild(canvas);
     }
-  } else {
-    // Use DataTransfer interface to access the file(s)
-    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-    }
+
+  }
+} else {
+  // Use DataTransfer interface to access the file(s)
+  for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+    console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
   }
 }
-
-
-function dragOverHandler(ev) {
-  console.log('File(s) in drop zone');
-
-  // Prevent default behavior (Prevent file from being opened)
-  ev.preventDefault();
 }
 
+function dragOverHandler(ev) {
+console.log('File(s) in drop zone');
 
-function processImg(file) {
-  let src = cv.imread(file.name);
-  let dst = new cv.Mat();
-
-  // You can try more different parameters
-  cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY, 0);
-  cv.imshow('img_bw', dst);
-  src.delete(); 
-  dst.delete();
-
-  console.log('Finished Processing');
+// Prevent default behavior (Prevent file from being opened)
+ev.preventDefault();
 }
